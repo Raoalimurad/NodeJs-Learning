359ebd6ef23b97313c0fb3584f3e231cec0f560b
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-27 11:04:57 +0500

    Your commit message

--- app.js
+++ app.js
@@ -15,16 +15,33 @@
 
 // DATE 27/03/2024
 // GETTING INPUTS FORM COMMAND LINE
-const fs =require('fs')         /////fs means file system
+// const fs =require('fs')         /////fs means file system
 
 // fs.writeFileSync(process.argv[2],process.argv[3])
 
 // appply condition on it
-if(process.argv[2] == 'add'){
-    fs.writeFileSync(process.argv[3],process.argv[4])
-}else if(process.argv[2] == 'remove'){
-    fs.unlinkSync(process.argv[3])
-}else{
-    console.log('invalid input')
-}
+// if(process.argv[2] == 'add'){
+//     fs.writeFileSync(process.argv[3],process.argv[4])
+// }else if(process.argv[2] == 'remove'){
+//     fs.unlinkSync(process.argv[3])
+// }else{
+//     console.log('invalid input')
+// }
 
+
+//  FILE LIST 
+
+// making file
+const fs =require('fs')  
+const path = require('path') ////for taking path
+const filePath = path.join(__dirname,'files') ////////to give me directory path
+for(var i = 0;i<5;i++){
+    
+    fs.writeFileSync(`${filePath}/hello${i}.txt`, 'a simple text file');
+
+}
+// read file names
+fs.readdir(filePath,(err,file)=>{
+   console.log(file)
+   
+})
