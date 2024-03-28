c6530cad0ee1b222553ba77c0a0cb6b1a4974e14
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-28 10:45:38 +0500

    Your commit message

--- app.js
+++ app.js
@@ -50,8 +50,8 @@
 // CRUD WITH FILE SYSTEM
 
 
-const fs =require('fs') 
-const path = require('path')  
+// const fs =require('fs') 
+// const path = require('path')  
 
 // const dirPath = path.join(__dirname,'crud');
 // const filePath = `${dirPath}/myworld.txt`;
@@ -84,4 +84,35 @@ const path = require('path')
 
 /////Delete file method 
 
-fs.unlinkSync(filePath)
+// fs.unlinkSync(filePath)
+
+
+
+// DATE 28/3/2024
+
+//  ASYNCHRONOUS DATA
+
+let a = 10 ;
+let b = 12 ;
+setTimeout(()=>{   ////it take time that's why value will not update
+  b = 40            //////e drawbacks of asynchronus 
+},2000)
+
+console.log(a+b) ////output is 22 beacuse it call first 
+
+
+///HANDLE ASYNCHRONOUS DATA
+
+let handleData = new Promise((resolve, reject) => {
+ 
+  setTimeout(()=>{
+    b = 40
+    resolve(b)
+  },2000)
+   
+})
+handleData.then((data)=>{
+  b = data
+  console.log(a+b)
+})
+
