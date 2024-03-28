70890987d11ab463520d133e9c58fdae7d00d334
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-28 09:07:51 +0500

    Your commit message

--- app.js
+++ app.js
@@ -32,16 +32,39 @@
 //  FILE LIST 
 
 // making file
-const fs =require('fs')  
-const path = require('path') ////for taking path
-const filePath = path.join(__dirname,'files') ////////to give me directory path
-for(var i = 0;i<5;i++){
+// const fs =require('fs')  
+// const path = require('path')                            ////for taking path
+// const filePath = path.join(__dirname,'files')         ////////to give me directory path
+// for(var i = 0;i<5;i++){
     
-    fs.writeFileSync(`${filePath}/hello${i}.txt`, 'a simple text file');
+//     fs.writeFileSync(`${filePath}/hello${i}.txt`, 'a simple text file');
 
-}
-// read file names
-fs.readdir(filePath,(err,file)=>{
-   console.log(file)
+// }
+// // read file names
+// fs.readdir(filePath,(err,file)=>{
+//    console.log(file)
    
+// })
+
+
+// CRUD WITH FILE SYSTEM
+
+
+const fs =require('fs') 
+const path = require('path')  
+
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/myworld.txt`;
+
+// create file
+fs.writeFileSync(filePath,'this is my good file')        ////it will create file in given path folder
+
+
+/////read file 
+
+fs.readFile(filePath,'utf8',(err,item)=>{                 ////////it will read file 
+  console.log(item)
 })
+
+/////Delete file method 
+
