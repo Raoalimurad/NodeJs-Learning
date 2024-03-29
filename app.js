e7e05c03c2e3b8384d720eab14a8b8785f462011
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-03-29 09:36:45 +0500

    Your commit message

--- app.js
+++ app.js
@@ -92,27 +92,28 @@
 
 //  ASYNCHRONOUS DATA
 
-let a = 10 ;
-let b = 12 ;
-setTimeout(()=>{   ////it take time that's why value will not update
-  b = 40            //////e drawbacks of asynchronus 
-},2000)
+// let a = 10 ;
+// let b = 12 ;
+// setTimeout(()=>{   ////it take time that's why value will not update
+//   b = 40            //////e drawbacks of asynchronus 
+// },2000)
 
-console.log(a+b) ////output is 22 beacuse it call first 
+// console.log(a+b) ////output is 22 beacuse it call first 
 
 
-///HANDLE ASYNCHRONOUS DATA
+// ///HANDLE ASYNCHRONOUS DATA
 
-let handleData = new Promise((resolve, reject) => {
+// let handleData = new Promise((resolve, reject) => {
  
-  setTimeout(()=>{
-    b = 40
-    resolve(b)
-  },2000)
+//   setTimeout(()=>{
+//     b = 40
+//     resolve(b)
+//   },2000)
    
-})
-handleData.then((data)=>{
-  b = data
-  console.log(a+b)
-})
+// })
+// hgfgandleData.then((data)=>{
+//   b = data
+//   console.log(a+b)
+// })
 
+///// this is
---
+++ main.js
@@ -0,0 +1,47 @@
+// const express = require('express')
+// const app = express()
+
+
+////make routes using express js
+
+// app.get('/',(req,res)=>{
+//     res.send("this is home page")
+//     console.log(req.query.name)
+// })
+// app.listen(`3000`)
+// app.get('/about',(req,res)=>{
+//     res.send("this is beautiful about page")
+// })
+
+
+////render html and json data
+
+
+
+// app.get('/',(req,res)=>{
+//         res.send({
+//             name:"raoali",
+//             email:'raoalimurad@gmail.com'
+//         })
+   
+//     })
+//     app.listen('3000')
+
+//     app.get('/about', (req, res) => {
+//         const pageContent = `
+//             <h1>This is the About Page</h1>
+//             <input type="text" placeholder="Username" />
+//         `;
+//         res.send(pageContent);
+//     });
+    
+
+
+const express = require('express')
+const path = require("path")
+const app = express()
+const filePath = path.join(__dirname,"files")
+console.log(filePath)
+
+app.use(express.static(filePath))
+app.listen('3000')
