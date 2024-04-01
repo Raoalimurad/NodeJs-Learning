ca60fe8122fc6dbd6e4c8e4f79cc43d997c741c6
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-04-01 17:21:18 +0500

    Your commit message

--- getApi.js
+++ getApi.js
@@ -1,10 +1,42 @@
 const express = require('express')
 const dbConnect = require('./connection')
+const mongodb = require('mongodb')
 const app = express()
+
+app.use(express.json()) /////middleware
 app.get('/', async (req, res) => {
     let data = await dbConnect()
     let result =await data.find().toArray()
    
     res.send(result)
 })
+
+
+/////post method
+
+app.post('/', async (req,res)=>{
+    console.log(req.body)
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body)
+    res.send(result)
+    
+})
 app.listen(3000)
+
+ /////update method
+
+ app.put('/:name',async (req,res)=>{
+    let data = await dbConnect();
+    let result = data.updateOne(
+        {name:req.params.name}
+        ,{$set:req.body}
+    )
+   res.send("updated")
+ })
+ /// delete method
+
+ app.delete('/:id',async (req,res)=>{
+    let data = await dbConnect();
+    let result = await data.deleteOne({ _id: new mongodb.ObjectId(req.params.id) })
+    res.send("done")
+ })
