b09a0913510c7f13d797e10d4133a6aa94a3e2b3
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-04-03 11:26:03 +0500

    Your commit message

---
+++ index1.js
@@ -0,0 +1,47 @@
+const express = require('express')
+require('./config')
+const product = require('./productScehma')
+const app = express()
+
+app.use(express.json()); ////MIDDLEWARE
+
+
+///POST API
+app.post('/', async (req,res)=>{
+    console.log(req.body)
+    let data =new product(req.body)
+    let result = await data.save()
+    res.send(result)
+})
+
+//GET API
+app.get('/',async(req,res)=>{
+    let data = await product.find()
+ res.send(data)
+})
+
+
+////DELETE API
+app.delete('/delete/:_id',async(req,res)=>{
+    let data = await product.deleteOne(req.params)
+    res.send(data)
+})
+
+
+
+///////update API
+
+app.put('/update/:name',async(req,res)=>{
+    console.log(req.params)
+    console.log(req.body)
+    let data = await product.updateOne(
+        req.params,{$set:req.body}
+        )
+    res.send(data)
+})
+
+
+
+
+
+app.listen(3000)
